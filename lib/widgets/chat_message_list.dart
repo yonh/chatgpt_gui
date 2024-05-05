a25457d8fedc069b70fe236ba7cda44df9dc8b4c
yongheng <az456005@163.com>
2024-05-05 20:21:46 +0800

    调整消息头像样式

--- lib/widgets/chat_message_list.dart
+++ lib/widgets/chat_message_list.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:markdown_widget/config/markdown_generator.dart';
 
@@ -30,7 +31,10 @@ class ChatMessageList extends HookConsumerWidget {
     return ListView.separated(
       controller: listController,
       itemBuilder: (context, index) {
-        return MessageItem(message: messages[index]);
+        final msg = messages[index];
+        return msg.isUser
+            ? SentMessageItem(message: msg)
+            : ReceivedMessageItem(message: msg);
       },
       itemCount: messages.length, // 消息数量
       separatorBuilder: (context, index) => const Divider(
@@ -41,8 +45,100 @@ class ChatMessageList extends HookConsumerWidget {
   }
 }
 
-class MessageItem extends StatelessWidget {
-  const MessageItem({super.key, required this.message});
+class ReceivedMessageItem extends StatelessWidget {
+  const ReceivedMessageItem({super.key, required this.message});
+
+  final Message message;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      // 右键菜单
+      onSecondaryTapDown: (details) {
+        showMenu(
+          context: context,
+          position: RelativeRect.fromLTRB(
+            details.globalPosition.dx,
+            details.globalPosition.dy,
+            details.globalPosition.dx,
+            details.globalPosition.dy,
+          ),
+          items: [
+            PopupMenuItem(
+              padding: EdgeInsets.only(left: 10, right: 0),
+              height: 30,
+              child: Text('Copy'),
+              value: 'Copy',
+            ),
+          ],
+        ).then((value) {
+          if (value == 'Copy') {
+            Clipboard.setData(ClipboardData(text: message.content));
+          }
+        });
+      },
+      // 长按复制
+      onLongPress: () {
+        Clipboard.setData(ClipboardData(text: message.content));
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('Copied to clipboard'),
+            duration: const Duration(milliseconds: 1000),
+          ),
+        );
+      },
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          CircleAvatar(
+              // backgroundImage: NetworkImage(
+              //   'https://picsum.photos/40/40',
+              // ),
+              backgroundColor: Colors.white,
+              // foregroundColor: Colors.white,
+              child: Container(
+                alignment: Alignment.center,
+                width: 40,
+                height: 40,
+                child: SvgPicture.asset(
+                  'assets/images/openai.svg',
+                  width: 40,
+                  height: 40,
+                ),
+              )),
+          const SizedBox(width: 8),
+          // Expanded(
+          //   child: Column(
+          //     crossAxisAlignment: CrossAxisAlignment.start,
+          //     children: [
+          //       Text(
+          //         message.isUser ? 'You' : 'GPT',
+          //         style: Theme.of(context).textTheme.labelLarge,
+          //       ),
+          //       Text(
+          //         // 'This is a message',
+          //         message.content,
+          //         style: Theme.of(context).textTheme.bodyMedium,
+          //       ),
+          //     ],
+          //   ),
+          // ),
+          Flexible(
+            child: Container(
+              margin: const EdgeInsets.only(top: 5, right: 10),
+              // child: Text(message.content),
+              child: MessageContentWidget(message: message),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class SentMessageItem extends StatelessWidget {
+  const SentMessageItem({super.key, required this.message});
 
   final Message message;
 
@@ -93,8 +189,7 @@ class MessageItem extends StatelessWidget {
             // ),
             backgroundColor: message.isUser ? Colors.blue : Colors.grey,
             foregroundColor: Colors.white,
-            child: Text(message.isUser ? 'You' : 'GPT',
-                style: TextStyle(fontSize: 16)),
+            child: Text('Me', style: TextStyle(fontSize: 16)),
           ),
           const SizedBox(width: 8),
           // Expanded(
